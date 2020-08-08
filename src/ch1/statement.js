5b14f8ffa93dda75e4a2a7afb85201e749d82b0c
korkorna <korkorna@gmail.com>
2020-08-08 23:01:12 +0900

    extract function totalVolumeCredits

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -16,68 +16,74 @@ function statement(invoice, plays) {
         totalAmount += amountFor(perf);
 
     }
-    
-    let volumeCredits = 0;
-    for(let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-    }
+
+    let volumeCredits = totalVolumeCredits();
 
     result += `Amount owed is ${usd(totalAmount)}\n`;
 
     result += `You earned ${volumeCredits} credits\n`;
 
     return result;
-}
 
-function playFor(perf) {
-    return plays[perf.playID];
-}
 
-function volumeCreditsFor(aPerformance) {
-    let result = Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-}
+    function totalVolumeCredits() {
+        let volumeCredits = 0;
+        for (let perf of invoice.performances) {
+            volumeCredits += volumeCreditsFor(perf);
+        }
+        return volumeCredits;
+    }
 
-function usd(aNumber) {
-    return new Intl.NumberFormat("en-US",
-        {
-            style: "currency",
-            currency: "USD",
-            minimumFractionDigits: 2
-        }).format(aNumber/100);
-}
+    function playFor(perf) {
+        return plays[perf.playID];
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            {
+                style: "currency",
+                currency: "USD",
+                minimumFractionDigits: 2
+            }).format(aNumber/100);
+    }
 
 // #refactor 4 - 2 필요없어진 매개변수 제거
-function amountFor(aPerformance) {  //# 명확한 이름으로 변경
-    let result = 0;
-    // #refactor 4 - 1 매개변수를 쿼리 함수로 변경
-    switch(playFor(aPerformance).type) {
-        case "tragedy":
-            result = 40000;
+    function amountFor(aPerformance) {  //# 명확한 이름으로 변경
+        let result = 0;
+        // #refactor 4 - 1 매개변수를 쿼리 함수로 변경
+        switch(playFor(aPerformance).type) {
+            case "tragedy":
+                result = 40000;
 
-            if(aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
+                if(aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
 
-            break;
+                break;
 
-        case "comedy":
-            result = 30000;
+            case "comedy":
+                result = 30000;
 
-            if(aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
+                if(aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
 
-            result += 300 * aPerformance.audience;
+                result += 300 * aPerformance.audience;
 
-            break;
+                break;
 
-        default:
-            // #refactor 4 - 1 매개변수를 쿼리 함수로 변경
-            throw new Error(`Unknown type: ${playFor(aPerformance).type}`);
+            default:
+                // #refactor 4 - 1 매개변수를 쿼리 함수로 변경
+                throw new Error(`Unknown type: ${playFor(aPerformance).type}`);
+        }
+        return result;
     }
-    return result;
 }
 
 // Run the code
