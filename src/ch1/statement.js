325d599c9501369bd9df3a13d48a27400fedf03e
korkorna <korkorna@gmail.com>
2020-08-08 22:02:16 +0900

    Extract function amountFor

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -21,33 +21,8 @@ function statement(invoice, plays) {
     for(let perf of invoice.performances) {
 
         const play = plays[perf.playID];
-
-        let thisAmount = 0;
-
-        switch(play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-
-                if(perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-
-                break;
-
-            case "comedy":
-                thisAmount = 30000;
-
-                if(perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-
-                thisAmount += 300 * perf.audience;
-
-                break;
-
-            default:
-                throw new Error(`Unknown type: ${play.type}`);
-        }
+        // #refactor 1 - 함수 추출하기
+        let thisAmount = amountFor(perf, play);
 
         // Add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -66,6 +41,35 @@ function statement(invoice, plays) {
     result += `You earned ${volumeCredits} credits\n`;
 
     return result;
+}
+
+function amountFor(perf, play) {
+    let thisAmount = 0;
+    switch(play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+
+            if(perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+
+            break;
+
+        case "comedy":
+            thisAmount = 30000;
+
+            if(perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+
+            thisAmount += 300 * perf.audience;
+
+            break;
+
+        default:
+            throw new Error(`Unknown type: ${play.type}`);
+    }
+    return thisAmount;
 }
 
 // Run the code
