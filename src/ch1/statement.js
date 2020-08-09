6aa9e7422b20aec74a679711e208f53148a99ebb
korkorna <korkorna@gmail.com>
2020-08-09 10:10:24 +0900

    movig function

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -14,57 +14,58 @@ function statement(invoice, plays) {
         statementData.totalVolumeCredits = totalVolumeCredits(statementData);
         statementData.totalAmount = totalAmount(statementData);
         return statementData;
-    }
 
-    function enrichPerformance(aPerformance) {
-        const result = Object.assign({}, aPerformance);     // shallow copy
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch(aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if(aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if(aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`Unknown type: ${aPerformance.play.type}`);
+        function enrichPerformance(aPerformance) {
+            const result = Object.assign({}, aPerformance);     // shallow copy
+            result.play = playFor(result);
+            result.amount = amountFor(result);
+            result.volumeCredits = volumeCreditsFor(result);
+            return result;
         }
-        return result;
+
+        function playFor(aPerformance) {
+            return plays[aPerformance.playID];
+        }
+
+        function amountFor(aPerformance) {
+            let result = 0;
+            switch(aPerformance.play.type) {
+                case "tragedy":
+                    result = 40000;
+                    if(aPerformance.audience > 30) {
+                        result += 1000 * (aPerformance.audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    result = 30000;
+                    if(aPerformance.audience > 20) {
+                        result += 10000 + 500 * (aPerformance.audience - 20);
+                    }
+                    result += 300 * aPerformance.audience;
+                    break;
+                default:
+                    throw new Error(`Unknown type: ${aPerformance.play.type}`);
+            }
+            return result;
+        }
+
+        function volumeCreditsFor(aPerformance) {
+            let result = Math.max(aPerformance.audience - 30, 0);
+            if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+            return result;
+        }
+
+        function totalVolumeCredits(statementData) {
+            return statementData.performances
+                .reduce((total, p) => total + p.volumeCredits, 0);
+        }
+
+        function totalAmount(statementData) {
+            return statementData.performances
+                .reduce((total, p) => total + p.amount, 0);
+        }
     }
 
-    function volumeCreditsFor(aPerformance) {
-        let result = Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }
-
-    function totalVolumeCredits(statementData) {
-        return statementData.performances
-            .reduce((total, p) => total + p.volumeCredits, 0);
-    }
-
-    function totalAmount(statementData) {
-        return statementData.performances
-            .reduce((total, p) => total + p.amount, 0);
-    }
 }
 
 function renderPlainText(statementData, plays) {
