376b4bf456eac18e814341a4c67373e35bb9c1ef
korkorna <korkorna@gmail.com>
2020-08-17 16:41:50 +0900

    when noProducers, test shortfaill and profit

--- test/ch4/app.spec.js
+++ test/ch4/app.spec.js
@@ -8,13 +8,48 @@ describe('province', function () {
     beforeEach(function() {
         asia = new Province(sampleProvinceData()); //픽스쳐 설정
     });
+
     it ('shortfail', function() {
         assert.equal(asia.shortfall, 5); // 검증
-    }) ;
+    });
 
     it('profit', function() {
         assert.equal(asia.profit, 230);
-    })
+    });
+
+    it('change production', function() {
+       asia.producers[0].production = 20;
+       expect(asia.shortfall).equal(-6);
+       expect(asia.profit).equal(292);
+    });
+});
+
+describe('no producers', function() {
+    let noProducers;
+    beforeEach(function() {
+       const data = {
+           name: 'No Producers',
+           producers: [],
+           demand: 30,
+           price: 20
+       };
+       noProducers = new Province(data);
+    });
+
+    it ('shortfail', function() {
+        const data = {
+            name: 'No Producers',
+            producers: [],
+            demand: 30,
+            price: 20
+        };
+        noProducers = new Province(data);
+        assert.equal(noProducers.shortfall, 30);
+    });
+
+    it('profit', function() {
+        assert.equal(noProducers.profit, 0);
+    });
 });
 
 function sampleProvinceData() {
