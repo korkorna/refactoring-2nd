06a4696eed9af7d6702271a39ef18e51e7606789
korkorna <korkorna@gmail.com>
2020-08-17 16:24:51 +0900

    테스트 케이스 중복 제거

--- test/ch4/app.spec.js
+++ test/ch4/app.spec.js
@@ -4,13 +4,12 @@ const expect = require('chai').expect;
 const {Province, Producer} = require('../../src/ch4/app');
 
 describe('province', function () {
+    const asia = new Province(sampleProvinceData()); //픽스쳐 설정   // bad usecase  // 테스트끼리 상호작용하는 공유 픽스쳐로... 잘못된 결과를 내보냄..
     it ('shortfail', function() {
-        const asia = new Province(sampleProvinceData()); //픽스쳐 설정
         assert.equal(asia.shortfall, 5); // 검증
     }) ;
 
     it('profit', function() {
-        const asia = new Province(sampleProvinceData());
         assert.equal(asia.profit, 230);
     })
 });
