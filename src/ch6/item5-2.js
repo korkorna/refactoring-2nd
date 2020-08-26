9c2a9ec4cf0722796c0d6dec7ededabd3f61654d
korkorna <korkorna@gmail.com>
2020-08-26 16:55:27 +0900

    주석 추가

--- src/ch6/item5-2.js
+++ src/ch6/item5-2.js
@@ -6,6 +6,10 @@ module.exports = class Book {
         this._reservations = [];
     }
 
+    /**
+     * @deprecated addReservation 호출문을 모두 zz_addReservation 호출하도록 수정하라
+     * @param customer
+     */
     addReservation(customer) {
         this.zz_addReservation(customer, false);
     }
