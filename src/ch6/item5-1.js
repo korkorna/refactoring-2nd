f353cba529e6791660993b95de98789930aee238
korkorna <korkorna@gmail.com>
2020-08-26 16:31:59 +0900

    주석 추가

--- src/ch6/item5-1.js
+++ src/ch6/item5-1.js
@@ -4,6 +4,10 @@ function circumference(radius) {
     return 2 * Math.PI * radius;
 }
 
+/**
+ *
+ * @deprecate 'circum 함수는 앞으로 없어질 예정이니 circumference를 이용하세요.'
+ */
 module.exports = function circum(radius) {
     return circumference(radius);
 };
