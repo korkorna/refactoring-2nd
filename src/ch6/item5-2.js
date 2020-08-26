1974c891af926ee082156673361b7321dafb6f55
korkorna <korkorna@gmail.com>
2020-08-26 16:44:47 +0900

    새함수의 선언문과 호출문에 매개변수 추가

--- src/ch6/item5-2.js
+++ src/ch6/item5-2.js
@@ -6,10 +6,10 @@ module.exports = class Book {
     }
 
     addReservation(customer) {
-        this.zz_addReservation(customer);
+        this.zz_addReservation(customer, false);
     }
 
-    zz_addReservation(customer) {
+    zz_addReservation(customer, isPriority) {
         this._reservations.push(customer);
     }
 
