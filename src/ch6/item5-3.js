dc052dcb8eeb62f55642e40e208be5cd1e39bc72
korkorna <korkorna@gmail.com>
2020-08-26 17:10:40 +0900

    함수 본문을 함수 추출

--- src/ch6/item5-3.js
+++ src/ch6/item5-3.js
@@ -1,6 +1,10 @@
 'use strict';
 
+function xxNewEngland(stateCode) {
+    return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
+}
+
 module.exports = function isNewEngland(aCustomer) {
     const stateCode = aCustomer.address.state;
-    return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
+    return xxNewEngland(stateCode);
 }
