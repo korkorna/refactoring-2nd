706384c90b460643fb739779e2e134f1d94338d1
korkorna <korkorna@gmail.com>
2020-08-26 13:55:14 +0900

    불변 변수를 하나 선언하고 이름을 붙일 표현식의 복제본을 대입

--- src/ch6/item3.js
+++ src/ch6/item3.js
@@ -2,6 +2,7 @@
 
 module.exports = function price(order) {
     // 가격(price) = 기본 가격 - 수량 할인 + 배송비
+    const basePrice = order.quantity * order.itemPrice;
     return order.quantity * order.itemPrice -
         Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
         Math.min(order.quantity * order.itemPrice * 0.1, 100);
