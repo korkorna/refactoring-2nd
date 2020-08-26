93274949e50f6cea3a7046bcf7f66bc604116297
korkorna <korkorna@gmail.com>
2020-08-26 20:15:33 +0900

    새로 만든 객체를 readingOutsideRange 매개변수 추가

--- src/ch6/item8.js
+++ src/ch6/item8.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function readingOutsideRange(station, min, max) {
+module.exports = function readingOutsideRange(station, min, max, range) {
   return station.readings
     .filter(r => r.temp < min || r.temp > max);
 };
