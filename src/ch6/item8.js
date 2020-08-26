c4443b68a6f5242404d1077498fef80b6cdea32c
korkorna <korkorna@gmail.com>
2020-08-26 20:32:00 +0900

    온도 범위 체크는 NumberRange 객체로 위임

--- src/ch6/item8.js
+++ src/ch6/item8.js
@@ -2,5 +2,5 @@
 
 module.exports = function readingOutsideRange(station, range) {
   return station.readings
-    .filter(r => r.temp < range.min || r.temp > range.max);
+    .filter(r => !range.contains(r.temp));
 };
