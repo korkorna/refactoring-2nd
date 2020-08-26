a48cea741f5e295218cc6ab9ff5979e701a83b57
korkorna <korkorna@gmail.com>
2020-08-26 13:57:32 +0900

    원본 표현식을 새로 만든 변수로 교체

--- src/ch6/item3.js
+++ src/ch6/item3.js
@@ -4,7 +4,6 @@ module.exports = function price(order) {
     // 가격(price) = 기본 가격 - 수량 할인 + 배송비
     const basePrice = order.quantity * order.itemPrice;
     const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-    return basePrice -
-        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    return basePrice - quantityDiscount +
         Math.min(basePrice * 0.1, 100);
 }
