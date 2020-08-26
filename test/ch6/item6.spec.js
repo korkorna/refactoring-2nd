199357bd520e57244d309c72457e151b9e855b51
korkorna <korkorna@gmail.com>
2020-08-26 17:51:38 +0900

    testcase chnage defaultOwner

--- test/ch6/item6.spec.js
+++ test/ch6/item6.spec.js
@@ -4,10 +4,20 @@ let getDefaultOwner = require('../../src/ch6/item6').getDefaultOwner;
 const setDefaultOwner = require('../../src/ch6/item6').setDefaultOwner;
 
 describe('defaultOwner', function () {
+   let spaceship;
+   beforeEach(function() {
+      spaceship = {};
+   });
    it('success', function () {
-      let spaceship = {};
       spaceship.owner = getDefaultOwner();
       expect(spaceship.owner.firstName).equal('마틴');
       expect(spaceship.owner.lastName).equal('파울러');
+   });
+
+   it('change defaultOwner', function () {
+      setDefaultOwner({ firstName: '레베카', lastName: '파슨스' })
+      spaceship.owner = getDefaultOwner();
+      expect(spaceship.owner.firstName).equal('레베카');
+      expect(spaceship.owner.lastName).equal('파슨스');
    });
 });
