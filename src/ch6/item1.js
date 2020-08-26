9d9d78c81c265f01890cc6d445247bdf0ebff37c
korkorna <korkorna@gmail.com>
2020-08-26 13:14:26 +0900

    extract function

--- src/ch6/item1.js
+++ src/ch6/item1.js
@@ -18,15 +18,20 @@ function recordDueDate(invoice) {
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 }
 
+function calculateOutstanding(invoice) {
+    let outstanding = 0;
+    for (const o of invoice.orders) {
+        outstanding += o.amount;
+    }
+    return outstanding;
+}
+
 module.exports = function printOwing(invoice) {
 
     printBanner();
 
     // 미해결 채무(outstanding)를 계산한다.
-    let outstanding = 0;
-    for (const o of invoice.orders) {
-        outstanding += o.amount;
-    }
+    let outstanding = calculateOutstanding(invoice);
 
     recordDueDate(invoice);
     printDetails(invoice, outstanding);
