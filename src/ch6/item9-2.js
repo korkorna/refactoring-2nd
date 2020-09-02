e34b56c5525fd12b5869fb4d76fcbbfc2d65de52
korkorna <korkorna@gmail.com>
2020-09-02 09:40:46 +0900

    함수 추출

--- src/ch6/item9-2.js
+++ src/ch6/item9-2.js
@@ -3,6 +3,10 @@ const { acquireReading, baseRate, taxThreshold, Reading } = require('./item9');
 
 const rawReading = acquireReading();
 const aReading = new Reading(rawReading);
-const taxableCharge = Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
+const taxableCharge = taxableChargeFn();
+
+function taxableChargeFn() {
+    return Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
+}
 
 module.exports = taxableCharge;
