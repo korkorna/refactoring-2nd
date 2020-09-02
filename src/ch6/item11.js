53b010fb4e4ec823ef978a0f53d488097181bb76
korkorna <korkorna@gmail.com>
2020-09-02 18:33:00 +0900

    함수 추출(applyShipping)

--- src/ch6/item11.js
+++ src/ch6/item11.js
@@ -1,11 +1,16 @@
 'use strict';
 
 module.exports = function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-  const shippingPerCase = (basePrice > shippingMethod.discountThreshold) ?
-      shippingMethod.discountedFee : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+    const basePrice = product.basePrice * quantity;
+    const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
+    const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+    return price;
 };
+
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
+    const shippingPerCase = (basePrice > shippingMethod.discountThreshold) ?
+        shippingMethod.discountedFee : shippingMethod.feePerCase;
+    const shippingCost = quantity * shippingPerCase;
+    const price = basePrice - discount + shippingCost;
+    return price;
+}
