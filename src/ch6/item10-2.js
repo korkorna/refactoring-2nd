4b3a470bf02a0f386f9914cb7f04becd063853cb
korkorna <korkorna@gmail.com>
2020-09-02 10:27:45 +0900

    원본데이터를 변환함수로 변환하기(client2)

--- src/ch6/item10-2.js
+++ src/ch6/item10-2.js
@@ -1,7 +1,8 @@
 'use strict';
-const { acquireReading, baseRate, taxThreshold } = require('./item10');
+const { acquireReading, baseRate, taxThreshold, enrichReading } = require('./item10');
 
-const aReading = acquireReading();
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
 const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
 const taxableCharge = Math.max(0, base - taxThreshold(this.year));
 
