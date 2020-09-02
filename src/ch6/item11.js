d709e913dc908d3a7f33adf05a2ab896ffe26ea2
korkorna <korkorna@gmail.com>
2020-09-02 18:45:17 +0900

    함수추출(calculatePricingData)

--- src/ch6/item11.js
+++ src/ch6/item11.js
@@ -1,12 +1,17 @@
 'use strict';
 
 module.exports = function priceOrder(product, quantity, shippingMethod) {
-    const basePrice = product.basePrice * quantity;
-    const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-    const priceData = {basePrice:basePrice, quantity: quantity, discount: discount};
+    const priceData = calculatePricingData(product, quantity);
     const price = applyShipping(priceData, shippingMethod);
     return price;
 };
+
+function calculatePricingData(product, quantity) {
+    const basePrice = product.basePrice * quantity;
+    const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
+    const priceData = {basePrice: basePrice, quantity: quantity, discount: discount};
+    return priceData;
+}
 
 function applyShipping(priceData, shippingMethod) {
     const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold) ?
