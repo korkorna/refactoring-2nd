687286d1299fcded817c012e4f524bbff0b00f43
korkorna <korkorna@gmail.com>
2020-09-02 19:47:12 +0900

    junit test환경 및 성공 케이스 추가

--- src/ch6/orderCounter/src/main/java/splitphase/OrderCounter.java
+++ src/ch6/orderCounter/src/main/java/splitphase/OrderCounter.java
@@ -9,25 +9,25 @@ import java.util.stream.Stream;
 public class OrderCounter {
     public static void main(String[] args) {
         try {
-            run(args);
+            System.out.println(run(args));
         } catch (Exception e) {
             System.out.println(e);
             System.exit(1);
         }
     }
 
-    private static void run(String[] args) throws java.io.IOException {
+    static long run(String[] args) throws java.io.IOException {
         if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
         String filename = args[args.length -1];
         File input = Paths.get(filename).toFile();
         ObjectMapper mapper = new ObjectMapper();
         Order[] orders = mapper.readValue(input, Order[].class);
         if (Stream.of(args).anyMatch(arg -> "-r".equals(arg))) {
-            System.out.println(Stream.of(orders)
-                                    .filter(o -> "ready".equals(o.status))
-                                    .count());
+            return Stream.of(orders)
+                    .filter(o -> "ready".equals(o.status))
+                    .count();
         } else {
-            System.out.println(orders.length);
+            return orders.length;
         }
     }
 }
