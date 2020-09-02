0c2a0d6d7ca82dc811369fb3c05bb0d92d513fb6
korkorna <korkorna@gmail.com>
2020-09-02 18:49:43 +0900

    변수 인라인

--- src/ch6/item11.js
+++ src/ch6/item11.js
@@ -15,6 +15,5 @@ function applyShipping(priceData, shippingMethod) {
     const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold) ?
         shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = priceData.quantity * shippingPerCase;
-    const price = priceData.basePrice - priceData.discount + shippingCost;
-    return price;
+    return priceData.basePrice - priceData.discount + shippingCost;
 }
