ec0321c448671ddd3cbcc38456b29d772a281bc3
korkorna <korkorna@gmail.com>
2020-09-02 09:38:37 +0900

    base 계산식을 객체 함수(baseChare) 호출하도록 수정

--- src/ch6/item9-2.js
+++ src/ch6/item9-2.js
@@ -3,7 +3,6 @@ const { acquireReading, baseRate, taxThreshold, Reading } = require('./item9');
 
 const rawReading = acquireReading();
 const aReading = new Reading(rawReading);
-const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
-const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+const taxableCharge = Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
 
 module.exports = taxableCharge;
