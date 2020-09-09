82f16e53575b4f58f06b0fd79b910c46c13e04ec
korkorna <korkorna@gmail.com>
2020-09-09 12:23:45 +0900

    add ch7-3 example

---
+++ src/ch7/item3.js
@@ -0,0 +1,9 @@
+'use strict';
+
+class Order {
+    constructor(data) {
+        this.priority = data.priority;
+    }
+}
+
+module.exports = Order;
---
+++ test/ch7/item3.js
@@ -0,0 +1,23 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+
+const Order = require('../../src/ch7/item3');
+
+function highPriorityCount(aList) {
+    return aList
+        .filter(o => "high" === o.priority || "rush" === o.priority)
+        .length;
+}
+
+describe('replace primitive with object ', function () {
+    let orders;
+
+    beforeEach(function () {
+        orders = [{priority:'high'}, {priority:'low'}, {priority:'rush'}, {priority:'low'}]
+            .map(o => new Order(o));
+    });
+
+    it('우선순위 높은 주문 갯수 확인', function () {
+        expect(highPriorityCount(orders)).equal(2);
+    });
+});
