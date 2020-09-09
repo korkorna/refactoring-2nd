34afe8d58677d6d05d9d683e47a7bda63ab037de
korkorna <korkorna@gmail.com>
2020-09-09 10:18:23 +0900

    add ch1-2 example

---
+++ test/ch7/item1-2.spec.js
@@ -0,0 +1,24 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+
+const customerData = require('../../src/ch7/item1-2');
+
+describe('EncapsulateRecord(nested record)', function () {
+
+    let customerId, year, month;
+
+    beforeEach(function () {
+        customerId = '1920';
+        year = 2016;
+        month = 1;
+    });
+
+    it ('customer usage 읽기', function () {
+        expect(customerData[customerId].usages[year][month]).equal(50);
+    });
+
+    it ('customer usage 수정', function () {
+        customerData[customerId].usages[year][month] = 60;
+        expect(customerData[customerId].usages[year][month]).equal(60);
+    });
+});
