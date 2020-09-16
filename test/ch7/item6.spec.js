75889649dfc0a7757cbc0f7f855c1d986c3fc515
korkorna <korkorna@gmail.com>
2020-09-16 19:22:13 +0900

    add ch7-6/inlineClass

---
+++ test/ch7/item6.spec.js
@@ -0,0 +1,23 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+
+const { TrackingInformation, Shipment } = require('../../src/ch7/item6');
+
+describe('Inline Class TrackingInformation', function () {
+    let tranckingInformation,
+        shipment;
+
+    beforeEach(function () {
+        tranckingInformation = new TrackingInformation();
+        tranckingInformation.shippingCompany = 'CJ통운';
+        tranckingInformation.trackingNumber = 1111;
+
+        shipment = new Shipment();
+        shipment.trackingInformation = tranckingInformation;
+    })
+
+    it('vendor 조회', function () {
+        expect(shipment.trackingInfo).equal('CJ통운: 1111');
+    });
+});
+
