74a058fd2c6c01afef3487d3909e5f3295a27665
korkorna <korkorna@gmail.com>
2020-09-16 20:01:01 +0900

    department getter 삭제

--- src/ch7/item7.js
+++ src/ch7/item7.js
@@ -5,7 +5,6 @@ class Person {
         this._name = name;
     }
     get name() {return this._name;}
-    get department() {return this._department;}
     set department(arg) {this._department = arg;}
     get manager() {return this._department._manager;}
 }
