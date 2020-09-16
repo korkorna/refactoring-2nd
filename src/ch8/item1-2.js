40e2c9cac4b0e83ef391dc126971decca6be821d
korkorna <korkorna@gmail.com>
2020-09-16 12:39:40 +0900

    함수 인라인

--- src/ch8/item1-2.js
+++ src/ch8/item1-2.js
@@ -32,12 +32,8 @@ class Account {
 
     get bankCharge() {
         let result = 4.5;
-        if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+        if (this._daysOverdrawn > 0) result += this.type.overdraftCharge(this.daysOverdrawn);
         return result;
-    }
-    
-    get overdraftCharge() {
-        return this.type.overdraftCharge(this.daysOverdrawn);
     }
 
     get daysOverdrawn() {
