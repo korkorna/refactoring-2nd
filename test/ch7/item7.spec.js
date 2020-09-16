a63761ba6eaf70c8d3aeadfb3a6718baeb5878f5
korkorna <korkorna@gmail.com>
2020-09-16 19:57:32 +0900

    add ch7-7 example

---
+++ test/ch7/item7.spec.js
@@ -0,0 +1,24 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+
+const { Person, Department } = require('../../src/ch7/item7');
+
+describe('hideDelegate', function () {
+    let person;
+
+    beforeEach(function () {
+        let manager = new Person('매니저');
+        let department = new Department();
+        department.manager = manager;
+
+        person = new Person('마틴');
+        person.department = department;
+    })
+
+    it('manager 조회', function () {
+        const expected = '매니저';
+        expect(person.department.manager.name).equal(expected);
+    });
+
+});
+
