4c9f666633cf418dd7c93767e10f62d3b30b7d44
korkorna <korkorna@gmail.com>
2020-10-07 18:33:04 +0900

    변수 인라인

--- src/ch8/item8.js
+++ src/ch8/item8.js
@@ -2,15 +2,13 @@
 
 function acquireData(input) {
     const lines = input.split("\n");
-    const result = lines
-        .splice(1)
-        .filter(line => line.trim() != "")
-        .map(line => line.split(','))
-        .filter(record => record[1].trim() === "India")
-        .map(record => ({city: record[0].trim(), phone: record[2].trim()}))
+    return lines
+        .splice (1)
+        .filter (line => line.trim() != "")
+        .map    (line => line.split(','))
+        .filter (record => record[1].trim() === "India")
+        .map    (record => ({city: record[0].trim(), phone: record[2].trim()}))
     ;
-
-    return result;
 }
 
 module.exports = acquireData;
