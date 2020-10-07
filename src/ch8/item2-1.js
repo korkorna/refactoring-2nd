62e27b8b207d9efb720436192483d17c5bc27ef5
korkorna <korkorna@gmail.com>
2020-10-07 10:31:01 +0900

    discountRate 필드를 CustomerContract로 필드와 getter, setter 추가

--- src/ch8/item2-1.js
+++ src/ch8/item2-1.js
@@ -23,8 +23,17 @@ class Customer {
 }
 
 class CustomerContract {
-    constructor(startDate) {
+    constructor(startDate, discountRate) {
         this._startDate = startDate;
+        this._discountRate = discountRate;
+    }
+    
+    get discountRate() {
+        return this._discountRate;
+    }
+
+    set discountRate(value) {
+        this._discountRate = value;
     }
 }
 
