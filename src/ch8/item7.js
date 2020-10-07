2a8752191daa7f603041a6f6dfe3ef4e3c6698d9
korkorna <korkorna@gmail.com>
2020-10-07 17:46:15 +0900

    함수 추출

--- src/ch8/item7.js
+++ src/ch8/item7.js
@@ -1,15 +1,25 @@
 'use strict';
 
 function getYoungestAndTotalSalary(people) {
-    let youngest = people[0] ? people[0].age : Infinity;
-    let totalSalary = 0;
-    for (const p of people) {
-        totalSalary += p.salary;
-    }
-    for (const p of people) {
-        if (p.age < youngest) youngest = p.age;
-    }
+    let totalSalary = calcTotalSalary();
+    let youngest = getYoungest();
     return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+
+    function calcTotalSalary() {
+        let totalSalary = 0;
+        for (const p of people) {
+            totalSalary += p.salary;
+        }
+        return totalSalary;
+    }
+
+    function getYoungest() {
+        let youngest = people[0] ? people[0].age : Infinity;
+        for (const p of people) {
+            if (p.age < youngest) youngest = p.age;
+        }
+        return youngest;
+    }
 }
 
 module.exports = getYoungestAndTotalSalary;
