2d88ba83deeb30778208a28dbb3ab98774e88a29
korkorna <korkorna@gmail.com>
2020-10-07 17:42:10 +0900

    반복문 복제 및 부수효과 제거

--- src/ch8/item7.js
+++ src/ch8/item7.js
@@ -4,8 +4,10 @@ function getYoungestAndTotalSalary(people) {
     let youngest = people[0] ? people[0].age : Infinity;
     let totalSalary = 0;
     for (const p of people) {
-        if (p.age < youngest) youngest = p.age;
         totalSalary += p.salary;
+    }
+    for (const p of people) {
+        if (p.age < youngest) youngest = p.age;
     }
     return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
 }
