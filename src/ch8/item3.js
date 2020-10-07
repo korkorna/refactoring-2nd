7f582e0d5e8a16d6d6d4530209c6f10711315099
korkorna <korkorna@gmail.com>
2020-10-07 16:13:31 +0900

    함수 인라인 처리

--- src/ch8/item3.js
+++ src/ch8/item3.js
@@ -20,16 +20,10 @@ function photoDiv(p) {
 function zznew(p) {
     return [
         `<p>제목 : ${p.title}</p>`,
-        emitPhotoData(p),
+        `<p>위치 : ${p.location}</p>`,
+        `<p>날짜 : ${p.date.toDateString()}</p>`,
     ].join('\n');
 }
-
-function emitPhotoData(aPhoto) {
-    const result = [];
-    result.push(`<p>위치 : ${aPhoto.location}</p>`);
-    result.push(`<p>날짜 : ${aPhoto.date.toDateString()}</p>`);  // 제목출력
-    return result.join("\n");
-};
 
 function renderPhoto(photo) {
     return [];
