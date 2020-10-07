4f6201ac077c8aa8b5d9458d5edb458e69d6dd5e
korkorna <korkorna@gmail.com>
2020-10-07 15:37:15 +0900

    assertion을 추가하여 모든계좌의 이자유일이 계좌 타입에 부합한지 검증

--- src/ch8/item2-2.js
+++ src/ch8/item2-2.js
@@ -1,9 +1,11 @@
 'use strict';
+const assert = require('chai').assert;
 
 class Account {
     constructor(number, type, interestRate) {
         this._number = number;
         this._type = type;
+        assert(interestRate == this._type.interestRate);  // 충분히 검증이 된 이후 _type의 필드를 사용하도록 수정
         this._interestRate = interestRate;
     }
 
@@ -13,8 +15,13 @@ class Account {
 }
 
 class AccountType {
-    constructor(nameString) {
+    constructor(nameString, interestRate) {
         this._name = nameString;
+        this._interestRate = interestRate;
+    }
+
+    get interestRate() {
+        return this._interestRate;
     }
 }
 
--- test/ch8/item2-2.spec.js
+++ test/ch8/item2-2.spec.js
@@ -6,7 +6,7 @@ const { Account, AccountType } = require('../../src/ch8/item2-2');
 describe('MoveField-2', function () {
 
     it ('Account', function () {
-        const account = new Account(1, new AccountType("입출력통장"), 0.02);
+        const account = new Account(1, new AccountType("입출력통장", 0.02), 0.02);
         expect(account.interestRate).equal(0.02);
     });
 });
