51135b4def2e3ed750ae0563f93b08b1d019b497
korkorna <korkorna@gmail.com>
2020-10-07 15:38:29 +0900

    account에서 이자율을 accountType로 위임처리

--- src/ch8/item2-2.js
+++ src/ch8/item2-2.js
@@ -5,12 +5,10 @@ class Account {
     constructor(number, type, interestRate) {
         this._number = number;
         this._type = type;
-        assert(interestRate == this._type.interestRate);  // 충분히 검증이 된 이후 _type의 필드를 사용하도록 수정
-        this._interestRate = interestRate;
     }
 
     get interestRate() {
-        return this._interestRate;
+        return this._type.interestRate;
     }
 }
 
