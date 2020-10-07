305a13912c8167f77fa1cf00d61050241b56155f
korkorna <korkorna@gmail.com>
2020-10-07 18:26:49 +0900

    csv 데이터를 문자열배열로 데이터구조 변경

--- src/ch8/item8.js
+++ src/ch8/item8.js
@@ -5,10 +5,12 @@ function acquireData(input) {
     const result = [];
     const loopItems = lines
         .splice(1)
-        .filter(line => line.trim() != "");
+        .filter(line => line.trim() != "")
+        .map(line => line.split(','))
+    ;
 
     for (const line of loopItems) {
-        const record = line.split(",");
+        const record = line;
         if (record[1].trim() === "India") {
             result.push({city: record[0].trim(), phone: record[2].trim()});
         }
