c9fd131618f4603d76bafabcc475fd0331288292
korkorna <korkorna@gmail.com>
2020-10-07 18:31:37 +0900

    record 누적변수에 추가

--- src/ch8/item8.js
+++ src/ch8/item8.js
@@ -2,18 +2,14 @@
 
 function acquireData(input) {
     const lines = input.split("\n");
-    const result = [];
-    const loopItems = lines
+    const result = lines
         .splice(1)
         .filter(line => line.trim() != "")
         .map(line => line.split(','))
         .filter(record => record[1].trim() === "India")
+        .map(record => ({city: record[0].trim(), phone: record[2].trim()}))
     ;
 
-    for (const line of loopItems) {
-        const record = line;
-        result.push({city: record[0].trim(), phone: record[2].trim()});
-    }
     return result;
 }
 
