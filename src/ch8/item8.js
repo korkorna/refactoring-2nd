cbd396712aaabd991f26cff09ac03681e81dbde3
korkorna <korkorna@gmail.com>
2020-10-07 18:23:20 +0900

    반복문에서 빈줄 처리 로직을 filter로 처리

--- src/ch8/item8.js
+++ src/ch8/item8.js
@@ -4,9 +4,10 @@ function acquireData(input) {
     const lines = input.split("\n");
     const result = [];
     const loopItems = lines
-        .splice(1);
+        .splice(1)
+        .filter(line => line.trim() != "");
+
     for (const line of loopItems) {
-        if (line.trim() === "") continue;
         const record = line.split(",");
         if (record[1].trim() === "India") {
             result.push({city: record[0].trim(), phone: record[2].trim()});
