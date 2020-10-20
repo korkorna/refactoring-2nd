e46e26232c195ad2c6a79bc19a3b3aa461259a9b
korkorna <korkorna@gmail.com>
2020-10-20 16:07:17 +0900

    조건문 통합(isPartTime)

--- src/ch10/item2.js
+++ src/ch10/item2.js
@@ -2,8 +2,8 @@
 
 function disabilityAmount(anEmployee) {
     if (anEmployee.seniority < 2
-        || anEmployee.monthsDisabled > 12) return 0;
-    if (anEmployee.isPartTime) return 0;
+        || anEmployee.monthsDisabled > 12
+        || anEmployee.isPartTime) return 0;
     // 장애수당 계산
     let charge = 1;
     return charge;
