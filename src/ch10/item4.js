139c71da9c091be26cf1106d7256bad72cc86cdc
korkorna <korkorna@gmail.com>
2020-10-21 00:43:45 +0900

    airSpeedVelocity 오버라이딩 처리

--- src/ch10/item4.js
+++ src/ch10/item4.js
@@ -33,38 +33,35 @@ class Bird {
     constructor(birdObject) {
         Object.assign(this, birdObject);
     }
-
     get plumage() {   //깃털상태
         return "알 수 없다";
     }
-
     get airSpeedVelocity() {  // 비행 속도
-        switch (this.type) {
-            case '유럽 제비':
-                return 35;
-            case '아프리카 제비':
-                return 40 - 2 * this.numberOfCoconuts;
-            case '노르웨이 파랑 앵무':
-                return (this.isNailed) ? 0 : 10 + this.voltage / 10;
-            default:
-                return null;
-        }
+        return null;
     }
 }
-
 class EuropeanSwallow extends Bird{
     get plumage() {
         return '보통이다';
+    }
+    get airSpeedVelocity() {
+        return 35;
     }
 }
 class AfricanSwallow extends Bird{
     get plumage() {
         return (this.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
     }
+    get airSpeedVelocity() {
+        return 40 - 2 * this.numberOfCoconuts;
+    }
 }
 class NorwegianBlueParrot extends Bird{
     get plumage() {
         return (this.voltage > 1000) ? "그을렸다" : "예쁘다";
+    }
+    get airSpeedVelocity() {
+        return (this.isNailed) ? 0 : 10 + this.voltage / 10;
     }
 }
 
