f27454a8d216211b616836be7de391ecc897ed11
korkorna <korkorna@gmail.com>
2020-10-21 01:42:24 +0900

    함수명 변경

--- src/ch10/item4-2.js
+++ src/ch10/item4-2.js
@@ -46,14 +46,12 @@ class Rating {
         if (this.voyage.zone === "중국") result += 1;
         if (this.voyage.zone === "동인도") result += 1;
         result += this.historyLengthFactor;
-        result += this.voyageAndHistoryLengthFactor;
+        result += this.voyageLengthFactor;
         return result;
     }
 
-   get voyageAndHistoryLengthFactor() {
-        let result = 0;
-        if (this.voyage.length > 14) result -= 1;
-        return result;
+   get voyageLengthFactor() {
+        return this.voyage.length > 14 ? -1 : 0;
     }
 
     get historyLengthFactor() {
@@ -67,7 +65,7 @@ class ExperiencedChinaRating extends Rating {
         return Math.max(result, 0);
     }
 
-    get voyageAndHistoryLengthFactor() {
+    get voyageLengthFactor() {
         let result = 3;
         if (this.voyage.length > 12) result += 1;
         if (this.voyage.length > 18) result -= 1;
