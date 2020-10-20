1d59203277aeff41d68aa117265e7ebb188cd7fb
korkorna <korkorna@gmail.com>
2020-10-21 00:30:03 +0900

    bird의 슈퍼클래스 생성

--- src/ch10/item4.js
+++ src/ch10/item4.js
@@ -9,28 +9,42 @@ function speeds(birds) {
 }
 
 function plumage(bird) {   //깃털상태
-    switch (bird.type) {
-        case '유럽 제비':
-            return '보통이다';
-        case '아프리카 제비':
-            return (bird.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
-        case '노르웨이 파랑 앵무':
-            return (bird.voltage > 1000) ? "그을렸다" : "예쁘다";
-        default:
-            return "알 수 없다";
-    }
+    return new Bird(bird).plumage;
 }
 
 function airSpeedVelocity(bird) {  // 비행 속도
-    switch (bird.type) {
-        case '유럽 제비':
-            return 35;
-        case '아프리카 제비':
-            return 40 - 2 * bird.numberOfCoconuts;
-        case '노르웨이 파랑 앵무':
-            return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
-        default:
-            return null;
+    return new Bird(bird).airSpeedVelocity;
+}
+
+class Bird {
+    constructor(birdObject) {
+        Object.assign(this, birdObject);
+    }
+
+    get plumage() {   //깃털상태
+        switch (this.type) {
+            case '유럽 제비':
+                return '보통이다';
+            case '아프리카 제비':
+                return (this.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
+            case '노르웨이 파랑 앵무':
+                return (this.voltage > 1000) ? "그을렸다" : "예쁘다";
+            default:
+                return "알 수 없다";
+        }
+    }
+
+    get airSpeedVelocity() {  // 비행 속도
+        switch (this.type) {
+            case '유럽 제비':
+                return 35;
+            case '아프리카 제비':
+                return 40 - 2 * this.numberOfCoconuts;
+            case '노르웨이 파랑 앵무':
+                return (this.isNailed) ? 0 : 10 + this.voltage / 10;
+            default:
+                return null;
+        }
     }
 }
 
