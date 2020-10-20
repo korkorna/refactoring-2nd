eb1e30052273e8f1d612fa1e6ec792933be00abd
korkorna <korkorna@gmail.com>
2020-10-21 01:25:32 +0900

    조건부 블록을 함수 추출

--- src/ch10/item4-2.js
+++ src/ch10/item4-2.js
@@ -49,13 +49,18 @@ class Rating {
         let result = 2;
         if (this.voyage.zone === "중국") result += 1;
         if (this.voyage.zone === "동인도") result += 1;
+        result = this.voyageAndHistoryLengthFactor;
+        return result;
+    }
+
+   get voyageAndHistoryLengthFactor() {
+        let result = 0;
         if (this.voyage.zone === "중국" && this.hasChinaHistory(history)) {
             result += 3;
             if (this.history.length > 10) result += 1;
             if (this.voyage.length > 12) result += 1;
             if (this.voyage.length > 18) result -= 1;
-        }
-        else {
+        } else {
             if (this.history.length > 8) result += 1;
             if (this.voyage.length > 14) result -= 1;
         }
@@ -68,6 +73,7 @@ class ExperiencedChinaRating extends Rating {
         const result = super.captainHistoryRisk - 2;
         return Math.max(result, 0);
     }
+
 }
 
 module.exports = {
