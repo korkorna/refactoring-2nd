1c537ae95f539587e44ff55908e372981e0c80e4
korkorna <korkorna@gmail.com>
2020-10-21 01:39:09 +0900

    historyLengthFactor 함수 추출 및 서브 클래스 오버라이드

--- src/ch10/item4-2.js
+++ src/ch10/item4-2.js
@@ -51,9 +51,13 @@ class Rating {
 
    get voyageAndHistoryLengthFactor() {
         let result = 0;
-        if (this.history.length > 8) result += 1;
+        result += this.historyLengthFactor;
         if (this.voyage.length > 14) result -= 1;
         return result;
+    }
+
+    get historyLengthFactor() {
+        return this.history.length > 8 ? 1 : 0;
     }
 }
 
@@ -65,10 +69,13 @@ class ExperiencedChinaRating extends Rating {
 
     get voyageAndHistoryLengthFactor() {
         let result = 3;
-        if (this.history.length > 10) result += 1;
+        result += this.historyLengthFactor;
         if (this.voyage.length > 12) result += 1;
         if (this.voyage.length > 18) result -= 1;
         return result;
+    }
+    get historyLengthFactor() {
+        return this.history.length > 10 ? 1 : 0;
     }
 }
 
