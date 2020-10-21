1b787ac2c08cd92c379614e698ee85ab1e57f978
korkorna <korkorna@gmail.com>
2020-10-21 16:27:49 +0900

    Add ch10-5-2 example

---
+++ src/ch10/item5-2.js
@@ -0,0 +1,60 @@
+class Site {
+    constructor(aCustomer) {
+        this._customer = aCustomer;
+    }
+
+    get customer() {
+        return this._customer;
+    }
+}
+
+class Customer {
+    constructor(name, billingPlan, paymentHistory) {
+        this._name = name;
+        this._billgingPlan = billingPlan;
+        this._paymentHistory = paymentHistory;
+    }
+
+    get name() {
+        return this._name;
+    }
+    get billingPlan() {
+        return this._billingPlan;
+    }
+    set billingPlan(arg) {
+        this._billgingPlan = arg;
+    }
+    get paymentHistory() {
+        return this._paymentHistory;
+    }
+}
+
+class Client1 {
+    run () {
+        const aCustomer = site.customer;
+        // ... 수많은 코드 ...
+        let customerName;
+        if (aCustomer === "미확인 고객") customerName = "거주자";
+        else customerName = aCustomer.name;
+    }
+}
+
+class Client2 {
+    run () {
+        const aCustomer = site.customer;
+
+        const plan = (aCustomer === "미확인 고객") ?
+            registry.billingPlans.basic
+            : aCustomer.billingPlan;
+    }
+}
+
+class Client3 {
+    run() {
+        const aCustomer = site.customer;
+
+        const weeksDelinquent = (aCustomer === "미확인 고객") ?
+            0
+            : aCustomer.paymentHistory.weeksDelinquentInLastYear;
+    }
+}
