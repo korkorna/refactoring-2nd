7c23acaf4f645309898c3019f29a4cd548adcc9d
korkorna <korkorna@gmail.com>
2020-10-21 15:50:25 +0900

    미확인 고객인지 검사하는 isUnknow 메소드 추가

--- src/ch10/item5.js
+++ src/ch10/item5.js
@@ -40,6 +40,12 @@ class UnknownCustomer {
     }
 }
 
+function isUnkown(arg) {
+    if (!((arg instanceof Customer) || (arg === '미확인 고객')))
+        throw new Error('잘못된 값과 비교: <${arg}>');
+    return (arg === '미확인 고객');
+}
+
 class Client1 {
     run () {
         const aCustomer = site.customer;
