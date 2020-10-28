7ff209527df58365b66397dd7432e62b16db865d
korkorna <korkorna@gmail.com>
2020-10-28 17:16:15 +0900

    알고리즘 변경

--- src/ch10/item7.js
+++ src/ch10/item7.js
@@ -6,16 +6,7 @@ function sendAlert() {
 }
 
 function checkForMiscreant(people) {
-    for (const p of people) {
-        if (p === '조커') {
-            sendAlert();
-            return;
-        }
-        if (p === '사루만') {
-            sendAlert();
-            return;
-        }
-    }
+    if (people.some(p => ["조커", "사루만"].includes(p))) sendAlert();
 }
 
 module.exports = checkForMiscreant;
