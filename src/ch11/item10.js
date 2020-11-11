3d99917473b86edc7bc0ecf9fe32d8c62c5f6769
korkorna <korkorna@gmail.com>
2020-11-11 13:43:46 +0900

    죽은 코드 제거하기

--- src/ch11/item10.js
+++ src/ch11/item10.js
@@ -1,17 +1,5 @@
 'use strict';
 
-class ChargeCalculator {
-    constructor() {
-    }
-    get baseCharge() {
-        return this._customer.baseRate * this._usage;
-    }
-    charge(customer, usage, provider) {
-        const baseCharge = customer.baseRate * usage;
-        return baseCharge + provider.connectionCharge;
-    }
-}
-
 function charge(customer, usage, provider) {
     const baseCharge = customer.baseRate * usage;
     return baseCharge + provider.connectionCharge;
