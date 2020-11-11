56c85ebaf8c68ea6bf65e5d4ed55cc985ca3e84e
korkorna <korkorna@gmail.com>
2020-11-11 13:10:05 +0900

    지역변수(result)를 필드로 옮기기

--- src/ch11/item9.js
+++ src/ch11/item9.js
@@ -11,7 +11,7 @@ class Scorer {
         this._scoringGuide = scoringGuide;
     }
     execute() {
-        let result = 0;
+        this._result = 0;
         let healthLevel = 0;
         let highMedicalRiskFlag = false;
 
@@ -23,11 +23,11 @@ class Scorer {
         let certificationGrade = "regular";
         if (this._scoringGuide.stateWithLowCertification(this._candidate.originState)) {
             certificationGrade = "low";
-            result -= 5;
+            this._result -= 5;
         }
         // 비슷한 코드가 한참 이어짐
-        result -= Math.max(healthLevel - 5, 0);
-        return result;
+        this._result -= Math.max(healthLevel - 5, 0);
+        return this._result;
     }
 }
 
