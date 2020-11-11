8628eaca96ff834dae5639c784edbb0d5fb07817
korkorna <korkorna@gmail.com>
2020-11-11 13:41:15 +0900

    함수의 매개변수를 사용하도록 수정하라

--- src/ch11/item10.js
+++ src/ch11/item10.js
@@ -1,8 +1,7 @@
 'use strict';
 
 class ChargeCalculator {
-    constructor(customer, usage, provider) {
-        this._customer = customer;
+    constructor( usage, provider) {
         this._usage = usage;
         this._provider = provider;
     }
@@ -11,13 +10,13 @@ class ChargeCalculator {
         return this._customer.baseRate * this._usage;
     }
     charge(customer, usage, provider) {
-        const baseCharge = this._customer.baseRate * this._usage;
+        const baseCharge = customer.baseRate * this._usage;
         return baseCharge + this._provider.connectionCharge;
     }
 }
 
 function charge(customer, usage, provider) {
-    return new ChargeCalculator(customer, usage, provider).charge(customer, usage, provider);
+    return new ChargeCalculator(usage, provider).charge(customer, usage, provider);
 }
 
 module.exports = charge;
