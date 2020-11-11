5b982ebf70cc2c2b895f682d602ca299d26f8652
korkorna <korkorna@gmail.com>
2020-11-11 13:39:15 +0900

    생성자 매개변수를 함수로 옮기기

--- src/ch11/item10.js
+++ src/ch11/item10.js
@@ -10,14 +10,14 @@ class ChargeCalculator {
     get baseCharge() {
         return this._customer.baseRate * this._usage;
     }
-    get charge() {
+    charge(customer, usage, provider) {
         const baseCharge = this._customer.baseRate * this._usage;
         return baseCharge + this._provider.connectionCharge;
     }
 }
 
 function charge(customer, usage, provider) {
-    return new ChargeCalculator(customer, usage, provider).charge;
+    return new ChargeCalculator(customer, usage, provider).charge(customer, usage, provider);
 }
 
 module.exports = charge;
